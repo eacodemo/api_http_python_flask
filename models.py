9f81391c639d38c8c0a61763d2fb6e8a4bab94bf
eacodemo <eacodemo@gmail.com>
2024-02-29 12:58:48 -0500

    Agregando modelo de asociacionporyectousuario

--- models.py
+++ models.py
@@ -31,3 +31,13 @@ class Proyecto(db.Model):
 
   def __str__(self):
     return self.nombre + self.descripcion + self.fechaInicio
+  
+
+class AsociacionProyectoUsuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    idProyecto = db.Column(db.Integer, db.ForeignKey('proyecto.id'), nullable=False)
+    idUsuario = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    rol = db.Column(db.String(50), nullable=False)
+
+    def __str__(self):
+        return self.idProyecto + self.idUsuario + self.rol
