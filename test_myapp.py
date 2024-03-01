604785a39482b7eae9b62b07a0f1fe06bb81ebb9
eacodemo <eacodemo@gmail.com>
2024-03-01 17:14:11 -0500

    Se agrego mas test unitarios

--- test_myapp.py
+++ test_myapp.py
@@ -1,8 +1,9 @@
 import unittest
 from flask import Flask
-from myapp import app, db  # Asegúrate de ajustar según tu estructura de archivos y nombres
+from myapp import app, db  
 from models import User, Proyecto, AsociacionProyectoUsuario, HistoriaUsuario, Tarea
-
+from flask import (
+  Flask, request, jsonify)
 
 class MyAppTestCase(unittest.TestCase):
 
@@ -19,7 +20,6 @@ class MyAppTestCase(unittest.TestCase):
     def test_get_all_users(self):
         response = self.app.get('/usuario')
         self.assertEqual(response.status_code, 200)
-        # Aquí puedes agregar más aserciones según tu lógica
 
     def test_get_specific_user(self):
         user = User(nombreUsuario='TestUser', email='test@example.com', password='password', tipo='admin')
@@ -28,9 +28,42 @@ class MyAppTestCase(unittest.TestCase):
 
         response = self.app.get(f'/usuario/{user.id}')
         self.assertEqual(response.status_code, 200)
-        # Otras aserciones según tu lógica
+    
+    def test_create_user(self):
+        user_data = {
+            'nombreUsuario': 'TestUser',
+            'email': 'test@example.com',
+            'password': 'password',
+            'tipo': 'admin'
+        }
 
-    # Agrega más pruebas para otras funciones
+        response = self.app.post('/usuario', json=user_data)
+        self.assertEqual(response.status_code, 200)  # 
+       
+
+    def test_update_user(self):
+        user = User(nombreUsuario='TestUser', email='test@example.com', password='password', tipo='admin')
+        db.session.add(user)
+        db.session.commit()
+
+        updated_data = {
+            'nombreUsuario': 'UpdatedUser',
+            'email': 'updated@example.com',
+            'password': 'updated_password',
+            'tipo': 'updated'
+        }
+
+        response = self.app.put(f'/usuario/{user.id}', data=updated_data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_user(self):
+        user = User(nombreUsuario='TestUser', email='test@example.com', password='password', tipo='admin')
+        db.session.add(user)
+        db.session.commit()
+
+        response = self.app.delete(f'/usuario/{user.id}')
+        self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
