d0b20b7e5cbc90319e7596c3d038f4837a0f802b
eacodemo <eacodemo@gmail.com>
2024-03-01 16:52:59 -0500

    Documentcion modelos

--- models.py
+++ models.py
@@ -2,9 +2,11 @@ from settings import db
 from sqlalchemy import Column, Integer, Text
 from datetime import datetime
 
-
-class User(db.Model):
-
+# Definición del modelo de Usuario
+class User(db.Model,):
+    def get_id(self):
+        return str(self.id)
+    
     id = db.Column(db.Integer, primary_key=True)
     nombreUsuario = db.Column(db.String(60) )
     email = db.Column(db.String(60))
@@ -19,20 +21,23 @@ class User(db.Model):
     def __str__(self):
         return self.nombreUsuario + self.email + self.tipo
 
+# Definición del modelo de Proyecto
 class Proyecto(db.Model):
     
-  id = db.Column(db.Integer, primary_key=True)
-  nombre = db.Column(db.String(255))
-  descripcion = db.Column(db.Text)
-  fechaInicio = db.Column(
-     db.TIMESTAMP, 
-     nullable=False, 
-     default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255))
+    descripcion = db.Column(db.Text)
+    fechaInicio = db.Column(
+        db.TIMESTAMP, 
+        nullable=False, 
+        default=datetime.utcnow)
 
-  def __str__(self):
-    return self.nombre + self.descripcion + self.fechaInicio
-  
+    def __str__(self):
+        return self.nombre + self.descripcion + self.fechaInicio
 
+
+
+# Definición del modelo de AsociacionProyectoUsuario
 class AsociacionProyectoUsuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     idProyecto = db.Column(db.Integer, db.ForeignKey('proyecto.id'), nullable=False)
@@ -43,6 +48,7 @@ class AsociacionProyectoUsuario(db.Model):
         return self.idProyecto + self.idUsuario + self.rol
     
 
+# Definición del modelo de HistoriaUsuario
 class HistoriaUsuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     Detalles = db.Column(db.Text)
@@ -55,6 +61,7 @@ class HistoriaUsuario(db.Model):
         return self.Detalles + self.CriteriosAceptacion + self.Estado + self.IdProyecto + self.IdTarea
     
 
+# Definición del modelo de Tarea
 class Tarea(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     descripcion = db.Column(db.Text)
@@ -63,3 +70,4 @@ class Tarea(db.Model):
 
     def __str__(self):
         return self.descripcion + self.estado + str(self.idHistoriaUsuario)
+ 
